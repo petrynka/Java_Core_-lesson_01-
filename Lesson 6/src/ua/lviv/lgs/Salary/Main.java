8c8bf711aa2c2152044d25ff5be6755ba4bb4f64
Arsen Petrynka <petrynla.arsen@gmail.com>
2022-01-03 08:55:56 +0200

    Homework 8 commit

--- Lesson 6/src/ua/lviv/lgs/Salary/Main.java
+++ Lesson 6/src/ua/lviv/lgs/Salary/Main.java
@@ -1,5 +1,8 @@
 package ua.lviv.lgs.Salary;
 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -9,6 +12,13 @@ public class Main {
 		
 		wpr.salary();
 		wpm.salary();
+		
+		// Random rnd = new Random();
+	      //  int number = rnd.nextInt(6) + 1;
+	        //System.out.println("Random number: " + number);
+	        
+	        int rand = ThreadLocalRandom.current().nextInt(30,37);
+	    	System.out.println("Value: " + rand);
 
 	}
 
